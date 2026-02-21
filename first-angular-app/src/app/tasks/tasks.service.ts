a1d4e6934851997cf4aa60c984bea4b2a951a468
dibyajyoti_47 <dibyajyoti.6025@gmail.com>
2026-02-21 16:48:04 +0530

    sec 2 compl

--- first-angular-app/src/app/tasks/tasks.service.ts
+++ first-angular-app/src/app/tasks/tasks.service.ts
@@ -27,6 +27,13 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem("tasks");
+    if(tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   public getUserTasks( userId: string) {
     return  this.tasks.filter((task) => task.userId === userId);
   }
@@ -39,11 +46,18 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date
     });
+    this.saveTasks();
   }
 
   public removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => {
       task.id !== id;
     });
+    this.saveTasks();
   }
+
+  private saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
+  }
+
 }
