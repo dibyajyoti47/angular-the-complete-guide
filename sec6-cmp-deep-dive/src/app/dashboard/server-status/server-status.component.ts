0ea11a997aa0655bc58e975b7283625db79be409
dibyajyoti_47 <dibyajyoti.6025@gmail.com>
2026-03-18 18:32:30 +0530

    lecture 124 compl

---
+++ sec6-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-status.component.html',
+  styleUrl: './server-status.component.css'
+})
+export class ServerStatusComponent implements OnInit{
+  currentStatus: "online" | "offline" | "unknown" = 'offline';
+
+  constructor() {  
+  }
+
+  ngOnInit() {
+     setInterval(() => {
+      const random = Math.random();
+      if(random <0.5)
+        this.currentStatus = "online"
+      else if (random < 0.9) 
+        this.currentStatus = "online"
+      else
+        this.currentStatus = "unknown"      
+    }, 1000);
+  }
+}
